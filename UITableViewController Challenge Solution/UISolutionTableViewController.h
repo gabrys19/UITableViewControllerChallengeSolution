19d0958f276fc3af193aab0029b84c54ff756847
GabrielNiedzialek <gabrielniedzialek@gmail.com>
2015-04-03 22:31:33 +0200

    created project

--- UITableViewController Challenge Solution/UISolutionTableViewController.h
+++ UITableViewController Challenge Solution/UISolutionTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  UISolutionTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Gabriel Niedzialek on 03.04.2015.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface UISolutionTableViewController : UITableViewController
 
 @end
-
