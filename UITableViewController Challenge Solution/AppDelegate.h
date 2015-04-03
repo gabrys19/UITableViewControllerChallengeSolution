522efc1c40ce69241ff5249fc30f29b80bd86196
GabrielNiedzialek <gabrielniedzialek@gmail.com>
2015-04-03 22:03:04 +0200

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Gabriel Niedzialek on 03.04.2015.
+//  Copyright (c) 2015 Gabriel Niedzialek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Gabriel Niedzialek on 03.04.2015.
+//  Copyright (c) 2015 Gabriel Niedzialek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
